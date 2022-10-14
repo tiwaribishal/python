b95a4289cc00ede9aba022c1e878fc35fa4235b4
tiwaribishal <114800074+tiwaribishal@users.noreply.github.com>
2022-10-14 17:26:01 +0530

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,17 @@
+''''''
+
+
+marks = input("enter your marks :")
+marks =int(marks)
+
+if  marks > 90 and marks <=100 :
+    print("your marks is grade A")
+
+elif marks >80 and marks <= 90 :
+    print("your marks is grade B")
+
+elif marks >70 and marks <=80 :
+    print("your marks is grade C")
+
+else :print("not a valid number")
+     
