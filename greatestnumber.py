18018fa4b7b126d53c633400906a8fc71a606c2b
tiwaribishal <114800074+tiwaribishal@users.noreply.github.com>
2022-10-16 09:15:12 +0530

    Add files via upload

---
+++ greatestnumber.py
@@ -0,0 +1,11 @@
+'''take two inputs from user(int/float)
+print the greatest greater'''
+
+first = int(input("enter first number :"))
+second =int(input("enter second number :"))
+
+if first > second :
+    print("first is greater")
+
+elif second > first :
+    print ("second is greater")
