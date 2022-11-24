e7f4cfaa45144d4034f6363c92721538690098ac
tiwaribishal <114800074+tiwaribishal@users.noreply.github.com>
2022-11-24 23:56:03 +0530

    Add files via upload

---
+++ arrayspractice.py
@@ -0,0 +1,9 @@
+# take a user input print the first letter  of name and last name of name
+# if length of that input is greater than 3 character than print ing in prefix otherwise print as it is
+person1 = input("enter name of person")
+x=len(person1)
+if x>3:
+    print("ing"+person1)
+
+else : x<3 
+print(person1)
